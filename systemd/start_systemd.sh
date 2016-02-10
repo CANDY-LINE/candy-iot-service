4211a9a60b0472185a67cbdc0c45c0e8e5796f5e
Daisuke Baba <baba.daisuke@gmail.com>
2016-02-10 11:20:08 +0900

    Change USB data connection interface when the ECM adapter is missing

--- systemd/start_systemd.sh
+++ systemd/start_systemd.sh
@@ -4,13 +4,28 @@
 
 MODULE_SUPPORTED=0
 
+function try_to_change_usb_data_conn {
+  RET=`lsusb | grep 1ecb:0202`
+  RET=$?
+  if [ "${RET}" != "0" ]; then
+    return
+  fi
+  # Reset to default
+  # echo -e "AT@USBCHG=ACM\r\r" | microcom -t 1000 -s 115200 /dev/ttyUSB1 && echo "rebooting" && reboot
+  # Change to ECM
+  logger -s "Modifying the USB data connection I/F to ECM"
+  echo -e "AT@USBCHG=ECM\r\rAT@AUTOCONN=1\r\r" | microcom -t 1000 -s 115200 /dev/ttyACM0
+  logger -s "Rebooting..."
+  reboot
+}
+
 function diagnose_self {
   MODULE_IDS="1ecb:0208" # space delimiter
 
   RET=`dmesg | grep "register 'cdc_ether'"`
   RET=$?
   if [ "${RET}" != "0" ]; then
-    return
+    try_to_change_usb_data_conn
   fi
 
   for m in ${MODULE_IDS}
